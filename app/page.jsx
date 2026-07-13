7805d4646aeec29505de45f4af9ddbe76214c978
Jayde312 <rxvjayden@hotmail.com>
2026-07-13 23:28:28 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -75,38 +75,39 @@ export default function Home() {
               textAlign: "center",
             }}
           >
-            
-<h1
-  style={{
-    color: "#fff",
-    fontSize: 38,
-    lineHeight: 1.2,
-    marginBottom: 16,
-    textAlign: "center",
-    fontWeight: "normal",
-    fontFamily: "var(--font-inter), Arial Black", Arial, sans-serif",
-  }}
->
-  <div style={{ display: "inline-block", textAlign: "left" }}>
-    <span style={{ fontSize: 18, fontWeight: 400, display: "block" }}>
-      The
-    </span>
-    <span style={{ fontWeight: 700 }}>
-      {" "}
-    </span>
-    <span
-      style={{
-        color: "#cfa043",
-        fontWeight: 900,
-      }}
-    >
-     Retire Wealthy Guides
-    </span>
-  </div>
-</h1>
-
-
-
+            <h1
+              style={{
+                color: "#fff",
+                fontSize: 38,
+                lineHeight: 1.2,
+                marginBottom: 16,
+                textAlign: "center",
+                fontWeight: "normal",
+                fontFamily: "var(--font-playfair), Georgia, serif",
+              }}
+            >
+              <div style={{ display: "inline-block", textAlign: "left" }}>
+                <span
+                  style={{
+                    fontSize: 18,
+                    fontWeight: 400,
+                    display: "block",
+                    fontFamily: "var(--font-inter), sans-serif",
+                  }}
+                >
+                  The
+                </span>
+                <span style={{ fontWeight: 900 }}>Retire{" "}</span>
+                <span
+                  style={{
+                    color: "#cfa043",
+                    fontWeight: 900,
+                  }}
+                >
+                  Wealthy Guides
+                </span>
+              </div>
+            </h1>
 
             <p
               style={{
@@ -116,67 +117,79 @@ export default function Home() {
                 marginBottom: 36,
               }}
             >
-            Helping you build lasting wealth through education, discipline and smart opportunities.
-            <p>  Join us on WhatsApp or Telegram </p>
-
+              Helping you build lasting wealth through education, discipline
+              and smart opportunities.
+              <p> Join us on WhatsApp or Telegram </p>
             </p>
 
-            <div style={{ display: "flex", flexDirection: "column", gap: 12, marginBottom: 8 }}>
-            <Link
-              href={WHATSAPP_URL}
-              target="_blank"
+            <div
               style={{
-                background: "#16a34a",
-                color: "#ffffff",
-                padding: "16px",
-                borderRadius: 16,
-                textDecoration: "none",
-                fontWeight: 700,
-                fontSize: 16,
-                border: "2px solid #16a34a",
-                display: "inline-flex",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: 8,
-                
+                display: "flex",
+                flexDirection: "column",
+                gap: 12,
+                marginBottom: 8,
               }}
             >
-               <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                width="18" 
-                height="18" 
-                fill="currentColor" 
-                viewBox="0 0 24 24"
+              <Link
+                href={WHATSAPP_URL}
+                target="_blank"
+                style={{
+                  background: "#16a34a",
+                  color: "#ffffff",
+                  padding: "16px",
+                  borderRadius: 16,
+                  textDecoration: "none",
+                  fontWeight: 700,
+                  fontSize: 16,
+                  border: "2px solid #16a34a",
+                  display: "inline-flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: 8,
+                }}
               >
-                <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946C.06 5.348 5.397.01 12.008.01c3.202.001 6.212 1.246 8.477 3.516 2.266 2.27 3.507 5.28 3.505 8.484-.004 6.657-5.34 11.997-11.953 11.997-2.005-.001-3.973-.502-5.717-1.456L0 24zm6.59-4.846c1.66.986 3.288 1.474 4.825 1.475 5.4 0 9.795-4.39 9.798-9.789.002-2.618-1.017-5.08-2.87-6.934C16.49 2.052 14.04 1.029 11.5 1.029c-5.402 0-9.8 4.393-9.803 9.792-.001 1.629.433 3.219 1.258 4.631l-.995 3.635 3.731-.977zm12.181-5.395c-.303-.151-1.793-.884-2.071-.985-.279-.101-.482-.151-.684.151-.202.303-.783.985-.96 1.187-.177.202-.354.227-.657.076-1.396-.698-2.3-1.226-3.21-2.78-.24-.411.24-.382.687-1.275.076-.151.038-.284-.019-.39-.057-.101-.482-1.161-.66-1.589-.173-.417-.348-.36-.482-.367-.124-.006-.266-.007-.407-.007-.141 0-.37.053-.564.266-.194.213-.74.723-.74 1.761 0 1.038.754 2.039.859 2.181.106.142 1.484 2.266 3.597 3.177 1.472.635 2.12.723 2.879.614.475-.068 1.455-.595 1.658-1.167.202-.572.202-1.061.142-1.161-.06-.1-.22-.151-.523-.303z"/>
-              </svg>
-              Chat on WhatsApp
-            </Link>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946C.06 5.348 5.397.01 12.008.01c3.202.001 6.212 1.246 8.477 3.516 2.266 2.27 3.507 5.28 3.505 8.484-.004 6.657-5.34 11.997-11.953 11.997-2.005-.001-3.973-.502-5.717-1.456L0 24zm6.59-4.846c1.66.986 3.288 1.474 4.825 1.475 5.4 0 9.795-4.39 9.798-9.789.002-2.618-1.017-5.08-2.87-6.934C16.49 2.052 14.04 1.029 11.5 1.029c-5.402 0-9.8 4.393-9.803 9.792-.001 1.629.433 3.219 1.258 4.631l-.995 3.635 3.731-.977zm12.181-5.395c-.303-.151-1.793-.884-2.071-.985-.279-.101-.482-.151-.684.151-.202.303-.783.985-.96 1.187-.177.202-.354.227-.657.076-1.396-.698-2.3-1.226-3.21-2.78-.24-.411.24-.382.687-1.275.076-.151.038-.284-.019-.39-.057-.101-.482-1.161-.66-1.589-.173-.417-.348-.36-.482-.367-.124-.006-.266-.007-.407-.007-.141 0-.37.053-.564.266-.194.213-.74.723-.74 1.761 0 1.038.754 2.039.859 2.181.106.142 1.484 2.266 3.597 3.177 1.472.635 2.12.723 2.879.614.475-.068 1.455-.595 1.658-1.167.202-.572.202-1.061.142-1.161-.06-.1-.22-.151-.523-.303z" />
+                </svg>
+                Chat on WhatsApp
+              </Link>
 
-            <Link
-              href={TELEGRAM_URL}
-              target="_blank"
-              style={{
-                background: "#21BEDE",
-                color: "#fff",
-                padding: "15px",
-                borderRadius: 16,
-                textDecoration: "none",
-                fontWeight: 700,
-                fontSize: 16,
-                border: "2px solid #21BEDE",
-                display: "inline-flex",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: 8,
-              }}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-1-.65-.35-1 .22-1.59.15-.15 2.71-2.48 2.76-2.69.01-.03.01-.14-.07-.2-.08-.06-.19-.04-.27-.02-.11.02-1.93 1.23-5.46 3.62-.51.35-.98.53-1.39.51-.46-.01-1.35-.26-2.01-.48-.81-.27-1.46-.42-1.4-.88.03-.24.37-.49 1.03-.75 4.04-1.76 6.74-2.92 8.09-3.48 3.85-1.6 4.64-1.88 5.17-1.89.11 0 .37.03.54.17.14.12.18.28.2.45-.02.07-.02.13-.03.19z"/>
-              </svg>
-              Chat on Telegram
-            </Link>
-          </div>
+              <Link
+                href={TELEGRAM_URL}
+                target="_blank"
+                style={{
+                  background: "#21BEDE",
+                  color: "#fff",
+                  padding: "15px",
+                  borderRadius: 16,
+                  textDecoration: "none",
+                  fontWeight: 700,
+                  fontSize: 16,
+                  border: "2px solid #21BEDE",
+                  display: "inline-flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: 8,
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-1-.65-.35-1 .22-1.59.15-.15 2.71-2.48 2.76-2.69.01-.03.01-.14-.07-.2-.08-.06-.19-.04-.27-.02-.11.02-1.93 1.23-5.46 3.62-.51.35-.98.53-1.39.51-.46-.01-1.35-.26-2.01-.48-.81-.27-1.46-.42-1.4-.88.03-.24.37-.49 1.03-.75 4.04-1.76 6.74-2.92 8.09-3.48 3.85-1.6 4.64-1.88 5.17-1.89.11 0 .37.03.54.17.14.12.18.28.2.45-.02.07-.02.13-.03.19z" />
+                </svg>
+                Chat on Telegram
+              </Link>
+            </div>
 
             <section style={{ marginBottom: 42 }}>
               <h2
@@ -186,9 +199,7 @@ export default function Home() {
                   marginTop: 40,
                   marginBottom: 24,
                 }}
-              >
-                
-              </h2>
+              ></h2>
 
               <div
                 style={{
@@ -206,9 +217,9 @@ export default function Home() {
                     margin: 0,
                   }}
                 >
-                  The Retire Wealthy Guides (RWG) is a global community where people
-                  share retirement ideas, learn from one another, and find inspiration
-                  for the next chapter of life.
+                  The Retire Wealthy Guides (RWG) is a global community where
+                  people share retirement ideas, learn from one another, and
+                  find inspiration for the next chapter of life.
                 </p>
 
                 <p
@@ -219,10 +230,12 @@ export default function Home() {
                     margin: 0,
                   }}
                 >
-                  Our worldwide community is supported from the <strong>United States</strong>,
-                  <strong> Australia</strong>, and the <strong>United Kingdom</strong>,
-                  bringing together members from across the globe in a diverse environment
-                  where people from different cultures and backgrounds can connect, learn,
+                  Our worldwide community is supported from the{" "}
+                  <strong>United States</strong>,
+                  <strong> Australia</strong>, and the{" "}
+                  <strong>United Kingdom</strong>, bringing together members
+                  from across the globe in a diverse environment where people
+                  from different cultures and backgrounds can connect, learn,
                   and inspire one another.
                 </p>
 
@@ -234,14 +247,16 @@ export default function Home() {
                     margin: 0,
                   }}
                 >
-                  The Retire Wealthy Guides also unites top financial experts to help you
+                  The Retire Wealthy Guides also unites top financial experts
+                  to help you
                   <strong> invest smarter</strong>,
                   <strong> trade confidently</strong>,
                   <strong> eliminate debt</strong>,
-                  <strong> build wealth</strong>, and strengthen your retirement finances.
-                  With shared tools, free education, support, insights, and step-by-step
-                  guidance, we're here to help you grow residual income and achieve your
-                  long-term financial freedom and retirement goals.
+                  <strong> build wealth</strong>, and strengthen your
+                  retirement finances. With shared tools, free education,
+                  support, insights, and step-by-step guidance, we're here to
+                  help you grow residual income and achieve your long-term
+                  financial freedom and retirement goals.
                 </p>
               </div>
             </section>
