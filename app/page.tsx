bc50a5daccdda239289129f7a1885a15db4eb41e
Jamesgwi <jamesgwi@atomicmail.io>
2026-08-11 23:29:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export const dynamic = "force-dynamic";
 const theme = {
   bg: "#0A152E",
   bgDark: "#021212",
-  bgLight: "#0a2e2e",
+  bgLight: "#1C4A94",
   bgLighter: "#0f3d3d",
   gold: "#2dd4bf",
   goldLight: "#5eead4",
